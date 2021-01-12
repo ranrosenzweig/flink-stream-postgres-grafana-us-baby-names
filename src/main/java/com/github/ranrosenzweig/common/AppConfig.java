3d139f239b169cf2a32afd0b8a2ad1e7f392e2d7
Ran Rosenzweig <ranrosenzweig@gmail.com>
2021-01-12 11:55:29 +0200

    init commit

---
+++ src/main/java/com/github/ranrosenzweig/common/AppConfig.java
@@ -0,0 +1,15 @@
+package com.github.ranrosenzweig.common;
+
+public class AppConfig{
+    public static final String POSTGRES_USERNAME_CONFIG = "postgres.username";
+    public static final String POSTGRES_PASSWORD_CONFIG = "postgres.password";
+    public static final String POSTGRES_DRIVER_CLASS_CONFIG = "postgres.driverClass";
+    public static final String POSTGRES_HOSTNAME_CLASS_CONFIG = "postgres.hostname";
+    public static final String POSTGRES_PORT_CLASS_CONFIG = "postgres.port";
+    public static final String POSTGRES_DATABASE_CLASS_CONFIG = "postgres.database";
+
+    public static final String BATCH_CONFIG = "batch.config";
+
+
+
+}
